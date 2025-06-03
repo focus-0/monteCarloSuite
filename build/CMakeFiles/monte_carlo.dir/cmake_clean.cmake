5b03e962e981cff1f7487a8d6aa9d19a8e84a294
Ayush Lohumi <ayushlohumi@MacBook-Air-2.local>
2025-06-04 03:48:13 +0530

    built again

---
+++ build/CMakeFiles/monte_carlo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/monte_carlo.dir/src/monte_carlo.cpp.o"
+  "CMakeFiles/monte_carlo.dir/src/monte_carlo.cpp.o.d"
+  "monte_carlo"
+  "monte_carlo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/monte_carlo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
